d5d8b50917dd688b9d2cf8d725051e10242f86dc
thea-exe <csokkunthea@gmail.com>
2022-04-28 18:36:15 -0400

    initial navbar

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -17,55 +17,62 @@ module.exports = {
   public: 'docs/assets',
   themeConfig: {
     logo: '/koda-v3.png',
-    repo: 'https://github.com/kodadot/nft-gallery',
-    repoLabel: 'kodadot/nft-gallery',
     navbar: [
       {
-        text: 'Tutorials',
+        text: 'Overview',
         children: [
-          { 
-            text: 'Adding funds to your wallet', 
-            link: '/tutorials/how-to-top-up-wallet.md' 
+          {
+            text: 'Who We Are',
+            link: '',
           },
           {
-            text: 'Airdroping nfts',
-            link: '/tutorials/how-to-airdrop.md'
+            text: 'Tokens',
+            link: '/token/draft.md',
           },
           {
-            text: 'Buying NFTs from iOS device',
-            link: '/tutorials/how-to-kodadot-phone-ios.md',
+            text: 'Frequently Asked Questions',
+            link: '/FAQ.md',
           },
-          { 
-            text: 'Creating a wallet', 
-            link: '/tutorials/how-to-create-wallet.md' 
-          },
-          {
-            text: 'Filing bug report',
-            link: '/tutorials/how-to-bug-report.md'
-          },
-          {
-            text: 'Fixing keypair error',
-            link: '/tutorials/how-to-fix-error-keypair.md',
-          },
-          { 
-            text: 'Minting', 
-            link: '/tutorials/how-to-mint.md' 
-          },
-          {
-            text: 'NFTs from scratch',
-            link: '/tutorials/how-to-make-nft.md',
-          },
-          {
-            text: 'Translating KodaDot via Github',
-            link: '/tutorials/language-translations.md',
-          },
-        ],
+        ]
       },
       {
-        text: 'KodaDot',
+        text: 'Contribute',
         children: [
           {
-            text: 'Kodadot.xyz',
+            text: 'Why Contribute?',
+            link: '/developer.md',
+          },
+          {
+            text: 'Installation',
+            link: '/first_time.md',
+          },      
+          {
+            text: 'Contributing Guidelines',
+            link: 'https://github.com/kodadot/nft-gallery/blob/main/CONTRIBUTING.md'
+          },
+          {
+            text: 'Code of Conduct',
+            link: 'https://github.com/kodadot/nft-gallery/blob/main/CODE_OF_CONDUCT.md)'
+          },
+          {
+            text: 'Ambassadors',
+            link: '/ambassador-program/ambassador-intro.md'
+          },
+          {
+            text: 'Code of Conduct',
+            link: 'https://github.com/kodadot/nft-gallery/blob/main/CODE_OF_CONDUCT.md)'
+          },
+          {
+            text: 'Code of Conduct',
+            link: 'https://github.com/kodadot/nft-gallery/blob/main/CODE_OF_CONDUCT.md)'
+          },
+        ]
+      },
+      {
+        text: 'Find Us',
+        children: [
+          {
+            text: 'Kodadot',
             link: 'https://kodadot.xyz',
           },
           {
@@ -75,80 +82,6 @@ module.exports = {
           {
             text: 'Twitter',
             link: 'https://twitter.com/kodadot',
-          },
-          {
-            text: 'Presskit & Logo',
-            link: 'https://github.com/kodadot/kodadot-presskit',
-          },
-          {
-            text: 'Ambassador Program',
-            link: '/ambassador-program/ambassador-intro.md',
-          },
-          {
-            text: 'Collaboration with Artists',
-            link: '/get-featured.md',
-          },
-          {
-            text: 'Frequently asked questions',
-            link: '/faq.md',
-          },
-          {
-            text: 'Token $kodao',
-            link: '/token/draft.md',
-          },
-        ]
-      },
-      {
-        text: 'Incentives',
-        children: [
-          {
-            text: 'Incentives',
-            link: '/incentives/incentives.md',
-          },
-          {
-            text: 'Incentivized Bug Reporting',
-            link: '/incentives/incentivized-bug-reporting.md',
-          },
-          {
-            text: 'Incentivized Development',
-            link: '/developer.md',
-          },
-          {
-            text: 'Incentivized QA',
-            link: '/incentives/incentivized-qa.md',
-          },
-          {
-            text: 'Translating KodaDot',
-            link: '/incentives/translating-kodadot.md',
-          },
-        ]
-      },
-      {
-        text: 'KodaDot Development',
-        children: [
-          {
-            text: 'Why you should contribute to KodaDot?',
-            link: '/developer.md',
-          },
-          {
-            text: 'Meta_Hours (GitHub)',
-            link: 'https://github.com/kodadot/nft-gallery/discussions/categories/meta-hours'
-          },         
-          { 
-            text: 'nft-gallery/issues',
-            link: 'https://github.com/kodadot/nft-gallery/issues'
-          },
-          {
-            text: 'nft-gallery/pulls',
-            link: 'https://github.com/kodadot/nft-gallery/pulls' 
-          },
-          {
-            text: 'nft-gallery/code',
-            link: 'https://github.com/kodadot/nft-gallery'
-          },
-          {
-            text: 'Learn more about contributing',
-            link: 'https://github.com/kodadot/nft-gallery/blob/main/CONTRIBUTING.md'
           },
         ]
       }
